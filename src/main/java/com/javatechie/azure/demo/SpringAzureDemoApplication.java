d16e721bca46165344a2e396fbad824b90de6cdf
robert-liu-yu <liuyu12345@hotmail.com>
2024-10-19 13:44:19 -0400

    2nd commit

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -19,3 +19,9 @@ public class SpringAzureDemoApplication {
 	}
 
 }
+/*
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:robert-liu-yu/spring-azure-demo.git
+git push -u origin main
+ */
